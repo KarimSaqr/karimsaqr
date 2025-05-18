b0e8e011b3dd15204aca5e38351bda4e7276a0a6
iiGreenFOXYT <karemyossry147@gmail.com>
2025-05-19 01:50:05 +0300

    test

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "./node_modules/firebase/firestore";
+
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
