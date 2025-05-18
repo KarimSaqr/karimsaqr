e3c31e602301d157757e038f97d6f91e6a396767
iiGreenFOXYT <karemyossry147@gmail.com>
2025-05-19 02:05:06 +0300

    test

--- firebase.js
+++ firebase.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getFirestore } from "./node_modules/firebase/firestore";
-
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD5mTMiqi5x1_Xb8uNNKxWAluFrIUktnrI",
   authDomain: "karimyoussry-site.firebaseapp.com",
